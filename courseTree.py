ee977e4e2303b7b108d070101b20c92bbcd08b4d
Thaqib <thaqibdamani0@gmail.com>
2020-08-10 20:40:19 +0530

    Parsing with multiple lists of courses added

--- courseTree.py
+++ courseTree.py
@@ -25,18 +25,46 @@ class parseTree(object):
 ## Prefix operator
 
 grammar = nltk.CFG.fromstring("""
-    S -> BG | A | BG A | A BG
-    
+    S -> BG | A | BG A | A BG | BG I BG | P BG BG | BG COM BG | A I BG | P A BG    
     BG -> B A B
     B -> "("|")"
 
-    A -> C I C | P C C | I C | C I
+    A -> CG I CG | P CG CG | I CG | CG I | P CG I CG | CG
 
-    C -> "math145"|"math147"|"cs135"
-    I -> "or"|"and"
+    CG -> C COM N | C COM N COM N | C COM N COM N COM N | C | N
+    COM -> ","
+    C -> "math145"|"math147"|"cs135"|"pmath345" 
+    N -> "135" | "145" | "147" | "101" | "345" | "346" | "347"
+    I -> "or"|"and"|"/"
     P -> "one-of"|"all-of"
 """)
 sentence = input("Sentence: ").split()
+
+
+# Real EX: (PMATH345 and 346) or 347
+
+grammerString = """
+S -> ROOT
+
+ROOT -> BG OR BG | BG AND BG | OR BG BG | AND BG BG
+
+BG -> B P B | P
+
+B -> "("|")"
+
+P -> CL AND CL | CL OR CL | OR CL CL| AND CL CL
+
+AND -> "and"|"all-of"
+OR -> "or"|"one-of"|"/"
+
+CL -> C N | C N COM N | C N COM N COM N | C N COM N COM N COM N | C N COM N COM N COM N COM N  
+
+COM -> ","
+N -> "145"|"135"|"147"|"137"
+C -> "math"|"cs" 
+"""
+
+grammar2 = nltk.CFG.fromstring(grammerString)
 
 p = parseTree(grammar, sentence)
 print(sentence)
